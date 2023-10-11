508ddc7d1f2e848a3742d0e4464592b311a1c2a2
Taranchuk <nfhfyxer@gmail.com>
2023-10-11 20:59:23 +0300

    archoblade tasks

---
+++ 1.4/Source/VREA_DefOf.cs
@@ -0,0 +1,11 @@
+﻿using RimWorld;
+using Verse;
+
+namespace VREArchon
+{
+    [DefOf]
+    public static class VREA_DefOf
+    {
+        public static HediffDef VREA_Sear;
+    }
+}
---
+++ 1.4/Source/VREArchonMod.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using System.Linq;
+using UnityEngine;
+using Verse;
+
+namespace VREArchon
+{
+    public class VREArchonMod : Mod
+    {
+        public VREArchonMod(ModContentPack pack) : base(pack)
+        {
+			new Harmony("VREArchonMod").PatchAll();
+        }
+    }
+}
