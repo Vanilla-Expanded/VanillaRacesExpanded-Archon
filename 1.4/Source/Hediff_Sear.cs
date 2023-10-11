c8f9c954dbcdf6ec766d20c38455080ebb1afd56
Taranchuk <nfhfyxer@gmail.com>
2023-10-11 22:13:47 +0300

    fixes

---
+++ 1.4/Source/Hediff_Sear.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Linq;
+using Verse;
+using static RimWorld.FleshTypeDef;
+
+namespace VREArchon
+{
+    public class Hediff_Sear : Hediff_Injury
+    {
+        public override void Tended(float quality, float maxQuality, int batchPosition = 0)
+        {
+            base.Tended(quality, maxQuality, batchPosition);
+            var comp = this.TryGetComp<HediffComp_GetsPermanent>();
+            if (!comp.IsPermanent)
+            {
+                comp.IsPermanent = true;
+            }
+        }
+    }
+}
