923e4d058d4ab6dbf750c4f1bcf98a21119ed286
Taranchuk <nfhfyxer@gmail.com>
2024-02-12 13:35:56 +0200

    archon raid fix + mod option to change archon raid chance

--- 1.4/Source/Events/IncidentWorker_PsychicStorm.cs
+++ 1.4/Source/Events/IncidentWorker_PsychicStorm.cs
@@ -8,6 +8,11 @@ namespace VREArchon
     {
         public override bool TryExecuteWorker(IncidentParms parms)
         {
+            var archonFaction = Find.FactionManager.FirstFactionOfDef(VREA_DefOf.VRE_Archons);
+            if (archonFaction is null)
+            {
+                return false;
+            }
             var map = (parms.target as Map);
             map.weatherManager.TransitionTo(VREA_DefOf.VREA_PsychicStorm);
             GameConditionManager gameConditionManager = parms.target.GameConditionManager;
@@ -19,11 +24,12 @@ namespace VREArchon
                 SendStandardLetter(def.letterLabel, gameCondition.LetterText, def.letterDef, parms, LookTargets.Invalid);
             }
 
-            if (Rand.Chance(0.5f))
+            if (Rand.Chance(VREArchonSettings.archonRaidSpawnChanceInPsychicStorm))
             {
                 IncidentParms raidParms = StorytellerUtility.DefaultParmsNow(VREA_DefOf.VREA_ArchonRaid.category, parms.target);
-                Find.Storyteller.incidentQueue.Add(VREA_DefOf.VREA_ArchonRaid, Find.TickManager.TicksGame
-                    + Mathf.Min(gameCondition.duration / 2, (GenDate.TicksPerHour * new IntRange(4, 8).RandomInRange)), raidParms);
+                raidParms.faction = archonFaction;
+                var ticksToFire = Find.TickManager.TicksGame + Mathf.Min(gameCondition.duration / 2, (GenDate.TicksPerHour * new IntRange(4, 8).RandomInRange));
+                Find.Storyteller.incidentQueue.Add(VREA_DefOf.VREA_ArchonRaid, ticksToFire, raidParms);
             }
             return true;
         }
