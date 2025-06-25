ae75f358af8c99f3cbf24d0e9552513b3d5bedcd
juanosarg <31105173+juanosarg@users.noreply.github.com>
2025-06-25 15:19:35 +0200

    v2.01

--- 1.6/Source/HarmonyPatches/IncidentWorker_RaidEnemy_FactionCanBeGroupSource_Patch.cs
+++ 1.6/Source/HarmonyPatches/IncidentWorker_RaidEnemy_FactionCanBeGroupSource_Patch.cs
@@ -6,7 +6,7 @@ namespace VREArchon
     [HarmonyPatch(typeof(IncidentWorker_RaidEnemy), nameof(IncidentWorker_RaidEnemy.FactionCanBeGroupSource))]
     public static class IncidentWorker_RaidEnemy_FactionCanBeGroupSource_Patch
     {
-        public static void Postfix(IncidentWorker_RaidEnemy __instance, ref bool __result, Faction f, Map map, bool desperate = false)
+        public static void Postfix(IncidentWorker_RaidEnemy __instance, ref bool __result, Faction f, IncidentParms parms, bool desperate = false)
         {
             if (__result && f.def == VREA_DefOf.VRE_Archons && __instance is not IncidentWorker_ArchonRaid)
             {
